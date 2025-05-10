0ac380b95be39302ebba6dec0d973a544d473314
l-dhias <rpleoddiass@gmail.com>
2025-05-10 17:06:06 -0300

    fim do projeto

--- src/ex.js
+++ src/ex.js
@@ -1,1 +1,9 @@
 "use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(5, 4));
+console.log(saudar("Leonardo"));
--- src/ex.ts
+++ src/ex.ts
@@ -1,1 +1,12 @@
-10
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+
+console.log(multiplicar(5, 4)); 
+console.log(saudar("Leonardo")); 
